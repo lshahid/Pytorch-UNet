41a8fed6a6a70a6403c1fc1c6c05210c0dda1107
lshahid <lshahid@ad.engr.wisc.edu@ep-r023692.ep.wisc.edu>
2024-03-19 12:41:44 -0500

    Mask filter script. Need to test on all data.

---
+++ scripts/mask_filter.py
@@ -0,0 +1,54 @@
+"""
+This script filters out image-mask pairs on the sides of the
+field of view that do not have a bladder. This ensures that
+images with bladders are used for training.
+"""
+
+import argparse
+from pathlib import Path
+import os
+import imageio
+import numpy as np
+import shutil
+
+def filter(work_dir: str='..', thresh: int=10):
+
+    # Concatenate working directory to paths
+    dir_img_all = Path(work_dir) / 'data_all/imgs/'
+    dir_masks_all = Path(work_dir) / 'data_all/masks/'
+    dir_img_filtered = Path(work_dir) / 'data/imgs/'
+    dir_masks_filtered = Path(work_dir) / 'data/masks/'
+
+    # Iterate over all masks
+    for subject in os.listdir(dir_masks_all):
+        for mask_name in os.listdir(os.path.join(dir_masks_all, subject)):
+
+            # Import mask
+            mask = imageio.imread(os.path.join(dir_masks_all, subject, mask_name))
+            mask_arr = np.array(mask)
+
+            # Number of bladder pixels
+            num_pixel = np.count_nonzero(mask_arr)
+
+            if num_pixel >= thresh:
+
+                # Make directory for images and masks with bladder
+                Path(os.path.join(dir_img_filtered, subject)).mkdir(parents=True, exist_ok=True)
+                Path(os.path.join(dir_masks_filtered, subject)).mkdir(parents=True, exist_ok=True)
+
+                # Move images and masks
+                shutil.move(os.path.join(dir_img_all, subject, mask_name[:-4] + '.jpg'),
+                            os.path.join(dir_img_filtered, subject, mask_name[:-4] + '.jpg'))
+                shutil.move(os.path.join(dir_masks_all, subject, mask_name),
+                            os.path.join(dir_masks_filtered, subject, mask_name))
+
+# Command-line arguments
+def get_args():
+    parser = argparse.ArgumentParser(description='Filter out image-mask pairs that have a bladder.')
+    parser.add_argument('--work-dir', '-w', dest='wd', type=str, default='..', help='Working directory')
+    parser.add_argument('--threshold', '-t', type=int, default=10, help='Threshold for number of pixels')
+
+    return parser.parse_args()
+
+args = get_args()
+filter(work_dir=args.wd, thresh=args.threshold)
