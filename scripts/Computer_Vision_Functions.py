b27d4f879077c8f3bb07793178fb7a8a0c73e711
Juan Pablo Gonzalez-Pereira <49408590+JPGonzalezPereira@users.noreply.github.com>
2024-06-05 16:52:03 -0500

    Updated DICE Score Function

--- scripts/Computer_Vision_Functions.py
+++ scripts/Computer_Vision_Functions.py
@@ -3,9 +3,10 @@ import numpy as np
 import scipy as sc
 from skimage import measure
 from PIL import Image
-from glob import glob
-from PIL import Image
 from stl import mesh
+import imageio.v3 as iio
+
+epsilon = 1e-10
 
 def Horizontal_Flip(img_path,img_save_folder):
     img = Image.open(img_path)
@@ -42,10 +43,14 @@ def Hausdorff_Distance(original_mask_path,predicted_mask_path):
 def DICE_Coeff(original_mask_path,predicted_mask_path):
     Original = Image.open(original_mask_path)
     Predicted = Image.open(predicted_mask_path)
+
     Original = np.array(Original)
     Predicted = np.array(Predicted)
+    if np.count_nonzero(Original)!=0:
+        Original = np.where((Original==0)|(Original==1),Original^1,Original)
+    Predicted = Predicted / 255
     Intersection = np.sum(Original * Predicted)
-    DICE = (2. * Intersection) / (np.sum(Original) + np.sum(Predicted))
+    DICE = (2. * Intersection + epsilon)  / (np.sum(Original) + np.sum(Predicted) + epsilon)
     return DICE
 
 def Mask_to_Array(file_path):
@@ -60,6 +65,39 @@ def Mask_to_Array(file_path):
         array[indices_zero] = 1
 
     return array
+
+def Transform_Image_to_2D_FFT(file_path,image_save_path,desired_file_ext):
+    if file_path.split('.')[-1] == 'gif':
+        img = iio.imread(file_path)
+        Img_Avg = img[0].mean(axis=2)
+        Img_FFT = sc.fft.fft2(Img_Avg)
+        Img_FFT = sc.fft.fftshift(Img_FFT)
+        Img_FFT_Magnitude = np.log(abs(Img_FFT))
+        Normalized_Img_FFT_Magnitude = (255*Img_FFT_Magnitude/np.max(Img_FFT_Magnitude)).astype(np.uint8)
+        File_Name = (file_path.split('.')[-2]).split('/')[-1]
+        Save_Path = image_save_path + File_Name + '.' + desired_file_ext
+        return iio.imwrite(Save_Path,Normalized_Img_FFT_Magnitude)
+    else:
+        img = iio.imread(file_path)
+        Img_Avg = img.mean(axis=2)
+        Img_FFT = sc.fft.fft2(Img_Avg)
+        Img_FFT = sc.fft.fftshift(Img_FFT)
+        Img_FFT_Magnitude = np.log(abs(Img_FFT))
+        Normalized_Img_FFT_Magnitude = (255*Img_FFT_Magnitude/np.max(Img_FFT_Magnitude)).astype(np.uint8)
+        File_Name = (file_path.split('.')[-2]).split('/')[-1]
+        Save_Path = image_save_path + File_Name + '.' + desired_file_ext
+        return iio.imwrite(Save_Path,Normalized_Img_FFT_Magnitude)
+
+
+def Transform_2D_FFT_to_Image(Image_FFT,image_save_path):
+    if str(type(Image_FFT)[0])=='numpy.ndarray[0]':
+        ifft = sc.fft.ifftshift(Image_FFT)
+        ifft = sc.fft.ifft2(ifft)
+        Reconstructed_Img = np.abs(ifft)
+        return iio.imwrite(image_save_path,Reconstructed_Img)
+    else:
+        if Image_FFT.split('.')[-1] == 'gif':
+            img = iio.imread(Image_FFT)
 
 def Array_to_STL(array):
     
