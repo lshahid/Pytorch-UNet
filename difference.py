ebd1751bc3383a7b724d2db01b4a07ac6f4d51f5
Labib Shahid <lshahid@DMP-WK-DLGP903.medphysics>
2024-12-03 16:41:41 -0600

    Difference

--- difference.py
+++ difference.py
@@ -2,11 +2,12 @@ import os
 from PIL import Image
 import numpy as np
 import pdb
+import statistics
 
 # Masks directory
 # Note: Make sure file names are identical in both true and predicted masks directories
 true_masks_dir = 'true_masks/'
-pred_masks_dir = 'pred_masks/'
+pred_masks_dir = 'saunet_masks_5slices/'
 diff_masks_dir = 'diff_masks/'
 
 # Dice score
@@ -23,38 +24,6 @@ def DICE_Coeff(true_mask_arr, pred_mask_arr):
     Intersection = np.sum(true_mask_arr * pred_mask_arr)
     DICE = (2. * Intersection + epsilon) / (np.sum(true_mask_arr) + np.sum(pred_mask_arr) + epsilon)
     return DICE
-
-def dice(im1, im2, epsilon = 1e-6):
-    """
-    Computes the Dice coefficient, a measure of set similarity.
-    Parameters
-    ----------
-    im1 : array-like, bool
-        Any array of arbitrary size. If not boolean, will be converted.
-    im2 : array-like, bool
-        Any other array of identical size. If not boolean, will be converted.
-    Returns
-    -------
-    dice : float
-        Dice coefficient as a float on range [0,1].
-        Maximum similarity = 1
-        No similarity = 0
-        
-    Notes
-    -----
-    The order of inputs for `dice` is irrelevant. The result will be
-    identical if `im1` and `im2` are switched.
-    """
-    im1 = np.asarray(im1).astype(bool)
-    im2 = np.asarray(im2).astype(bool)
-
-    if im1.shape != im2.shape:
-        raise ValueError("Shape mismatch: im1 and im2 must have the same shape.")
-
-    # Compute Dice coefficient
-    intersection = np.logical_and(im1, im2)
-
-    return (2. * intersection.sum() + epsilon) / ((im1.sum() + im2.sum()) + epsilon)
 
 dice_dict = {}
 
@@ -81,8 +50,8 @@ for mask_name in sorted(os.listdir(true_masks_dir)):
     # Calculate dice score
     DICE_Score = DICE_Coeff(true_arr, pred_arr)
     Name = mask_name.split('.')[0]
-    dice_dict[Name] = [dice(true_arr, pred_arr), DICE_Coeff(true_arr, pred_arr)]
-    pdb.set_trace()
+    dice_dict[Name] = DICE_Score
+    # pdb.set_trace()
 
 pdb.set_trace()
 
