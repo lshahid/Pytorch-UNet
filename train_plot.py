b9d4e2e8c22d6bb5056097e8f2db2ba3204472f6
Labib Shahid <lshahid@DMP-WK-DLGP903.medphysics>
2024-12-05 18:40:54 -0600

    Plot criterion, dice, and total loss

--- train_plot.py
+++ train_plot.py
@@ -1,73 +1,199 @@
+import argparse
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
-data_train = pd.read_csv('data_train.csv')
-data_val = pd.read_csv('data_val.csv')
-data_train['Total Loss'] = data_train[' Criterion Loss'] + data_train[' Dice Loss']
-data_val['Total Loss'] = data_val[' Criterion Loss'] + data_val[' Dice Loss']
+def get_args():
+    parser = argparse.ArgumentParser(description='Plot loss curves from training U-Net')
+    parser.add_argument('--dir', metavar='DIR', help='Directory with training data', required=True)
+    parser.add_argument('--no-save', '-n', action='store_true', help='Do not save loss curve plot')
+    parser.add_argument('--viz', '-v', action='store_true', help='Visualize loss curve plot')
 
-n_epochs = 30
+    return parser.parse_args()
 
-# Calculate metrics from training and validation
-epoch_list = []
-train_max_list = []
-train_min_list = []
-train_avg_list = []
-val_max_list = []
-val_min_list = []
-val_avg_list = []
+if __name__ == '__main__':
+    args = get_args()
 
-for epoch in range(1, n_epochs + 1):
-    train_epoch_data = data_train.loc[data_train['Epoch'] == epoch]
-    val_epoch_data = data_val.loc[data_val['Epoch'] == epoch]
-    train_max_value = train_epoch_data['Total Loss'].max()
-    train_min_value = train_epoch_data['Total Loss'].min()
-    train_avg_value = train_epoch_data['Total Loss'].mean()
-    val_max_value = val_epoch_data['Total Loss'].max()
-    val_min_value = val_epoch_data['Total Loss'].min()
-    val_avg_value = val_epoch_data['Total Loss'].mean()
+    dir = args.dir
+    data_train_dir = os.path.join(dir, 'data_train.csv')
+    data_val_dir = os.path.join(dir, 'data_val.csv')
 
-    epoch_list.append(epoch)
-    train_max_list.append(train_max_value)
-    train_min_list.append(train_min_value)
-    train_avg_list.append(train_avg_value)
-    val_max_list.append(val_max_value)
-    val_min_list.append(val_min_value)
-    val_avg_list.append(val_avg_value)
+    data_train = pd.read_csv(data_train_dir)
+    data_val = pd.read_csv(data_val_dir)
+    # data_train['Total Loss'] = data_train[' Criterion Loss'] + data_train[' Dice Loss']
+    # data_val['Total Loss'] = data_val[' Criterion Loss'] + data_val[' Dice Loss']
 
-# Plot loss curves
-epoch_arr = np.array(epoch_list)
-train_max_arr = np.array(train_max_list)
-train_min_arr = np.array(train_min_list)
-train_avg_arr = np.array(train_avg_list)
-val_max_arr = np.array(val_max_list)
-val_min_arr = np.array(val_min_list)
-val_avg_arr = np.array(val_avg_list)
+    # Extract metrics from training and validation
+    n_epochs = int(max(data_train['Epoch']))
+    model_number = dir.split('_')[-1]
+    
+    epoch_list = []
 
-plt.figure(figsize=(12, 5))
-plt.subplot(1, 2, 1)
-plt.plot(epoch_arr, train_max_arr, label='Max')
-plt.plot(epoch_arr, train_avg_arr, label='Avg')
-plt.plot(epoch_arr, train_min_arr, label='Min')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.grid()
-plt.legend()
-plt.title('Training')
-plt.ylim((0, 2.5))
+    train_loss_criterion_max_list = []
+    train_loss_criterion_min_list = []
+    train_loss_criterion_mean_list = []
+    val_loss_criterion_max_list = []
+    val_loss_criterion_min_list = []
+    val_loss_criterion_mean_list = []
 
-plt.subplot(1, 2, 2)
-plt.plot(epoch_arr, val_max_arr, label='Max')
-plt.plot(epoch_arr, val_avg_arr, label='Avg')
-plt.plot(epoch_arr, val_min_arr, label='Min')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.grid()
-plt.legend()
-plt.title('Validation')
-plt.ylim((0, 2.5))
+    train_loss_dice_max_list = []
+    train_loss_dice_min_list = []
+    train_loss_dice_mean_list = []
+    val_loss_dice_max_list = []
+    val_loss_dice_min_list = []
+    val_loss_dice_mean_list = []
 
-plt.suptitle('Model 007')
-# plt.show()
-plt.savefig('model_007_train.png')
+    train_loss_total_max_list = []
+    train_loss_total_min_list = []
+    train_loss_total_mean_list = []
+    val_loss_total_max_list = []
+    val_loss_total_min_list = []
+    val_loss_total_mean_list = []
+
+    for epoch in range(1, n_epochs + 1):
+        train_epoch_data = data_train.loc[data_train['Epoch'] == epoch]
+        val_epoch_data = data_val.loc[data_val['Epoch'] == epoch]
+
+        train_loss_criterion_max_value = train_epoch_data['Criterion Loss'].max()
+        train_loss_criterion_min_value = train_epoch_data['Criterion Loss'].min()
+        train_loss_criterion_mean_value = train_epoch_data['Criterion Loss'].mean()
+        val_loss_criterion_max_value = val_epoch_data['Criterion Loss'].max()
+        val_loss_criterion_min_value = val_epoch_data['Criterion Loss'].min()
+        val_loss_criterion_mean_value = val_epoch_data['Criterion Loss'].mean()
+
+        train_loss_dice_max_value = train_epoch_data['Dice Loss'].max()
+        train_loss_dice_min_value = train_epoch_data['Dice Loss'].min()
+        train_loss_dice_mean_value = train_epoch_data['Dice Loss'].mean()
+        val_loss_dice_max_value = val_epoch_data['Dice Loss'].max()
+        val_loss_dice_min_value = val_epoch_data['Dice Loss'].min()
+        val_loss_dice_mean_value = val_epoch_data['Dice Loss'].mean()
+
+        train_loss_total_max_value = train_epoch_data['Total Loss'].max()
+        train_loss_total_min_value = train_epoch_data['Total Loss'].min()
+        train_loss_total_mean_value = train_epoch_data['Total Loss'].mean()
+        val_loss_total_max_value = val_epoch_data['Total Loss'].max()
+        val_loss_total_min_value = val_epoch_data['Total Loss'].min()
+        val_loss_total_mean_value = val_epoch_data['Total Loss'].mean()
+
+        epoch_list.append(epoch)
+
+        train_loss_criterion_max_list.append(train_loss_criterion_max_value)
+        train_loss_criterion_min_list.append(train_loss_criterion_min_value)
+        train_loss_criterion_mean_list.append(train_loss_criterion_mean_value)
+        val_loss_criterion_max_list.append(val_loss_criterion_max_value)
+        val_loss_criterion_min_list.append(val_loss_criterion_min_value)
+        val_loss_criterion_mean_list.append(val_loss_criterion_mean_value)
+
+        train_loss_dice_max_list.append(train_loss_dice_max_value)
+        train_loss_dice_min_list.append(train_loss_dice_min_value)
+        train_loss_dice_mean_list.append(train_loss_dice_mean_value)
+        val_loss_dice_max_list.append(val_loss_dice_max_value)
+        val_loss_dice_min_list.append(val_loss_dice_min_value)
+        val_loss_dice_mean_list.append(val_loss_dice_mean_value)
+
+        train_loss_total_max_list.append(train_loss_total_max_value)
+        train_loss_total_min_list.append(train_loss_total_min_value)
+        train_loss_total_mean_list.append(train_loss_total_mean_value)
+        val_loss_total_max_list.append(val_loss_total_max_value)
+        val_loss_total_min_list.append(val_loss_total_min_value)
+        val_loss_total_mean_list.append(val_loss_total_mean_value)
+
+    # Plot loss curves
+    epoch_arr = np.array(epoch_list)
+
+    train_loss_criterion_max_arr = np.array(train_loss_criterion_max_list)
+    train_loss_criterion_min_arr = np.array(train_loss_criterion_min_list)
+    train_loss_criterion_mean_arr = np.array(train_loss_criterion_mean_list)
+    val_loss_criterion_max_arr = np.array(val_loss_criterion_max_list)
+    val_loss_criterion_min_arr = np.array(val_loss_criterion_min_list)
+    val_loss_criterion_mean_arr = np.array(val_loss_criterion_mean_list)
+
+    train_loss_dice_max_arr = np.array(train_loss_dice_max_list)
+    train_loss_dice_min_arr = np.array(train_loss_dice_min_list)
+    train_loss_dice_mean_arr = np.array(train_loss_dice_mean_list)
+    val_loss_dice_max_arr = np.array(val_loss_dice_max_list)
+    val_loss_dice_min_arr = np.array(val_loss_dice_min_list)
+    val_loss_dice_mean_arr = np.array(val_loss_dice_mean_list)
+    
+    train_loss_total_max_arr = np.array(train_loss_total_max_list)
+    train_loss_total_min_arr = np.array(train_loss_total_min_list)
+    train_loss_total_mean_arr = np.array(train_loss_total_mean_list)
+    val_loss_total_max_arr = np.array(val_loss_total_max_list)
+    val_loss_total_min_arr = np.array(val_loss_total_min_list)
+    val_loss_total_mean_arr = np.array(val_loss_total_mean_list)
+
+    plt.figure(figsize=(12, 14))
+    plt.subplot(3, 2, 1)
+    plt.plot(epoch_arr, train_loss_criterion_max_arr, label='Max')
+    plt.plot(epoch_arr, train_loss_criterion_mean_arr, label='Mean')
+    plt.plot(epoch_arr, train_loss_criterion_min_arr, label='Min')
+    plt.xlabel('Epoch')
+    plt.ylabel('Criterion Loss')
+    plt.grid()
+    plt.legend()
+    plt.title('Training')
+    plt.ylim((0, 2.5))
+
+    plt.subplot(3, 2, 2)
+    plt.plot(epoch_arr, val_loss_criterion_max_arr, label='Max')
+    plt.plot(epoch_arr, val_loss_criterion_mean_arr, label='Mean')
+    plt.plot(epoch_arr, val_loss_criterion_min_arr, label='Min')
+    plt.xlabel('Epoch')
+    plt.ylabel('Criterion Loss')
+    plt.grid()
+    plt.legend()
+    plt.title('Validation')
+    plt.ylim((0, 2.5))
+
+    plt.subplot(3, 2, 3)
+    plt.plot(epoch_arr, train_loss_dice_max_arr, label='Max')
+    plt.plot(epoch_arr, train_loss_dice_mean_arr, label='Mean')
+    plt.plot(epoch_arr, train_loss_dice_min_arr, label='Min')
+    plt.xlabel('Epoch')
+    plt.ylabel('Dice Loss')
+    plt.grid()
+    plt.legend()
+    # plt.title('Training')
+    plt.ylim((0, 2.5))
+
+    plt.subplot(3, 2, 4)
+    plt.plot(epoch_arr, val_loss_dice_max_arr, label='Max')
+    plt.plot(epoch_arr, val_loss_dice_mean_arr, label='Mean')
+    plt.plot(epoch_arr, val_loss_dice_min_arr, label='Min')
+    plt.xlabel('Epoch')
+    plt.ylabel('Dice Loss')
+    plt.grid()
+    plt.legend()
+    # plt.title('Validation')
+    plt.ylim((0, 2.5))
+
+    plt.subplot(3, 2, 5)
+    plt.plot(epoch_arr, train_loss_total_max_arr, label='Max')
+    plt.plot(epoch_arr, train_loss_total_mean_arr, label='Mean')
+    plt.plot(epoch_arr, train_loss_total_min_arr, label='Min')
+    plt.xlabel('Epoch')
+    plt.ylabel('Total Loss')
+    plt.grid()
+    plt.legend()
+    # plt.title('Training')
+    plt.ylim((0, 2.5))
+
+    plt.subplot(3, 2, 6)
+    plt.plot(epoch_arr, val_loss_total_max_arr, label='Max')
+    plt.plot(epoch_arr, val_loss_total_mean_arr, label='Mean')
+    plt.plot(epoch_arr, val_loss_total_min_arr, label='Min')
+    plt.xlabel('Epoch')
+    plt.ylabel('Total Loss')
+    plt.grid()
+    plt.legend()
+    # plt.title('Validation')
+    plt.ylim((0, 2.5))
+
+    plt.suptitle('Model '+ model_number, fontsize=14)
+
+    if args.viz:
+        plt.show()
+
+    if not args.no_save:
+        plt.savefig(os.path.join(dir, ('model_' + model_number + '_train.png')))
