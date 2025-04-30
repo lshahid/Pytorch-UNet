9bf86f2c75258a6ef48f3ef73246d78d08e10850
Labib Shahid <lshahid@wisc.edu>
2025-04-30 15:56:44 -0500

    Cleanup SAU-Net post processing code

--- SAU_Postprocessing.py
+++ SAU_Postprocessing.py
@@ -1,34 +1,19 @@
-"""
-This script segements all images in a directory.
-"""
 import argparse
 import logging
 import os
 import pdb
-
 import numpy as np
 import matplotlib.image
-
 from PIL import Image
 
 
 def get_args():
     parser = argparse.ArgumentParser(description='SAU Implementation of Predicted Images')
-    #parser.add_argument('--model', '-m', default='MODEL.pth', metavar='FILE',
-    #                    help='Specify the file in which the model is stored')
     parser.add_argument('--input-dir', '-i', metavar='INPUT_DIR', help='Input image directory', required=True)
     parser.add_argument('--output-dir', '-o', metavar='OUTPUT_DIR', help='Output image directory', required=True)
     parser.add_argument('--n_timephases', '-t', type=int, help='Number of time phases', required=True)
     parser.add_argument('--n_slices', '-s',  type=int, help='Number of slices', required=True)
-    #parser.add_argument('--viz', '-v', action='store_true',
-    #                   help='Visualize the images as they are processed')
     parser.add_argument('--no-save', '-n', action='store_true', help='Do not save the output masks')
-    #parser.add_argument('--mask-threshold', '-t', type=float, default=0.5,
-    #                    help='Minimum probability value to consider a mask pixel white')
-    #parser.add_argument('--scale', '-s', type=float, default=0.5,
-    #                    help='Scale factor for the input images')
-    #parser.add_argument('--bilinear', action='store_true', default=False, help='Use bilinear upsampling')
-    #parser.add_argument('--classes', '-c', type=int, default=2, help='Number of classes')
     
     return parser.parse_args()
 
@@ -56,6 +41,7 @@ def mask_to_image(mask: np.ndarray, mask_values):
 
     return Image.fromarray(out)
 
+
 if __name__ == '__main__':
     args = get_args()
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
@@ -66,8 +52,6 @@ if __name__ == '__main__':
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    # in_files = args.input
-    # out_files = get_output_filenames(args)
     in_files = [os.path.join(input_dir, f) for f in sorted(os.listdir(input_dir)) if os.path.isfile(os.path.join(input_dir, f))]
     out_files = [os.path.join(output_dir, os.path.splitext(os.path.basename(f))[0] + '.gif') for f in in_files]
 
